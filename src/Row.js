9bd93549861a5c9c2ca40e0e502d4959583485bd
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-14 23:16:24 +0000

    just renaming stuff

--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Row({ title }) {
-  return <div className="row"></div>;
+  const [movies, setMovies] = useState([]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      {/* title */}
+      {/* container -> posters */}
+    </div>
+  );
 }
 
 export default Row;
