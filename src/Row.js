201daf6f8914bc4cd4ef05605644f5b266c5eb0c
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-14 23:52:51 +0000

    Setup Posters Rows

--- src/Row.js
+++ src/Row.js
@@ -1,24 +1,35 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./Row.css";
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
+  const base_url = "https://image.tmdb.org/t/p/original/";
 
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-      console.log(request);
+      setMovies(request.data.results);
       return request;
     }
 
     fetchData();
-  }, []);
+  }, [fetchUrl]);
 
   return (
     <div className="row">
       <h2>{title}</h2>
       {/* title */}
-      {/* container -> posters */}
+      <div className="row__posters">
+        {/* several container -> posters */}
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={`${base_url}${movie.name}`}
+          />
+        ))}
+      </div>
     </div>
   );
 }
