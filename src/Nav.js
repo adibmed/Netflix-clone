258b21b9583924fbe26f958768e500f4b0ca3658
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-15 01:43:38 +0000

    ADD show/hide navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
 
 function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       <img
         className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt=""
+      />
+      <img
+        className="nav__avatar"
         src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
         alt=""
       />
