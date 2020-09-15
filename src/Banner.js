d0026e837e9c9c670aa6a8f5e2a8fa829c0614b8
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-15 00:57:59 +0000

    ADD banner random  content data

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import requests from "./requests";
 
 function Banner() {
-  return <div className="banner"></div>;
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1>{movie?.name || movie?.title || movie?.original_name}</h1>
+
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+
+        <h1 className="banner__description">{movie?.overview}</h1>
+      </div>
+    </header>
+  );
 }
 
 export default Banner;
