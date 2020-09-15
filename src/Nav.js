6955ee446a2007ade1fdd3ac76f09f6517d28acc
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-15 01:30:24 +0000

    ADD Navbar Styling

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Nav() {
-  return <div></div>;
+  return (
+    <div className="nav">
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt=""
+      />
+    </div>
+  );
 }
 
 export default Nav;
