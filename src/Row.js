63b13bb1d78ab19495912ebc13844e0ec357cdf3
adibemohamed <mr.eladib.mohamed@gmail.com>
2020-09-15 00:36:49 +0000

    ADD: Header compo(Banner)

--- src/Row.js
+++ src/Row.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
   const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -12,7 +12,6 @@ function Row({ title, fetchUrl }) {
       setMovies(request.data.results);
       return request;
     }
-
     fetchData();
   }, [fetchUrl]);
 
@@ -24,8 +23,11 @@ function Row({ title, fetchUrl }) {
         {/* several container -> posters */}
         {movies.map((movie) => (
           <img
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={`${base_url}${movie.name}`}
           />
         ))}
